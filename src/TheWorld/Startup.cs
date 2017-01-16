2047a391763ea5cc6e3e37b6fce0153917fb34c3
Zemich <zemichgithub@gmail.com>
2017-01-17 00:26:54 +0100

    To and with "Serving Files in ASP.NET 5"

--- src/TheWorld/Startup.cs
+++ src/TheWorld/Startup.cs
@@ -21,17 +21,8 @@ namespace TheWorld
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole();
-
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("<html><body><h3>Hello World!</h3></body></html>");
-            });
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
         }
     }
 }
