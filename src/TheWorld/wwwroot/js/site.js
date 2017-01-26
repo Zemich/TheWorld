8f85a4a494c58f72a4760974e992e140fd3d6c07
Zemich <zemichgithub@gmail.com>
2017-01-27 00:05:12 +0100

    To and with: "Using Bower".

--- src/TheWorld/wwwroot/js/site.js
+++ src/TheWorld/wwwroot/js/site.js
@@ -1,3 +1,14 @@
 ﻿// site.js
-var ele = document.getElementById("username");
-ele.innerHTML = "Shawn Wildermuth";
+(function () {
+    var ele = document.getElementById("username");
+    ele.innerHTML = "Shawn Wildermuth";
+
+    var main = document.getElementById("main");
+    main.onmouseenter = function () {
+        main.style = "background: #888";
+    };
+
+    main.onmouseleave = function () {
+        main.style = "";
+    };
+})();
