792a961261ea4baf49eae78fb5114ab532000b62
Zemich <zemichgithub@gmail.com>
2017-01-29 00:59:14 +0100

    To and with: "Practical jQuery".

--- src/TheWorld/wwwroot/js/site.js
+++ src/TheWorld/wwwroot/js/site.js
@@ -1,14 +1,32 @@
 ﻿// site.js
 (function () {
-    var ele = document.getElementById("username");
-    ele.innerHTML = "Shawn Wildermuth";
+    //var ele = $("#username");
+    //ele.text("Shawn Wildermuth");
 
-    var main = document.getElementById("main");
-    main.onmouseenter = function () {
-        main.style = "background: #888";
-    };
+    //var main = $("#main");
+    //main.on("mouseenter", function () {
+    //    main.css("background-color", "#888");
+    //    //main.style = "background: #888";
+    //});
+    //main.on("mouseleave", function () {
+    //    main.css("background-color", "");
+    //    //main.style = "";
+    //});
 
-    main.onmouseleave = function () {
-        main.style = "";
-    };
+    //var menuItems = $("ul.menu li a");
+    //menuItems.on("click", function () {
+    //    var me = $(this);
+    //    alert(me.text());
+    //});
+
+    var $sidebarAndWrapper = $("#sidebar,#wrapper");
+
+    $("#sidebarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Show Sidebar");
+        } else {
+            $(this).text("Hide Sidebar");
+        }
+    });
 })();
