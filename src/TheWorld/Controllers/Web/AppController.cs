487ef8d93a58eee025a24ab34a4e2e7e9bcb5ccd
Zemich <zemichgithub@gmail.com>
2017-02-05 01:39:24 +0100

    To and with: "Using Tag Helpers".

---
+++ src/TheWorld/Controllers/Web/AppController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TheWorld.Controllers.Web
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
