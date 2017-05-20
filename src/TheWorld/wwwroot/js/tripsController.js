fa4c0792acf8b3eec9d491909e8de671d603685c
Zemich <zemichgithub@gmail.com>
2017-05-21 00:33:10 +0200

    To and with: "Creating Data with AngularJS".

--- src/TheWorld/wwwroot/js/tripsController.js
+++ src/TheWorld/wwwroot/js/tripsController.js
@@ -4,22 +4,44 @@
     angular.module("app-trips")
         .controller("tripsController", tripsController);
 
-    function tripsController() {
+    function tripsController($http) {
         var vm = this;
 
-        vm.trips = [{
-            name: "US Trip",
-            created: new Date()
-        },{
-            name: "World Trip",
-            created: new Date()
-        }];
+        vm.trips = [];
 
         vm.newTrip = {};
 
+        vm.errorMessage = "";
+        vm.isBusy = true;
+
+        $http.get("/api/trips")
+            .then(function (response) {
+                // Success
+                angular.copy(response.data, vm.trips)
+            }, function (error) {
+                // Failure
+                vm.errorMessage = "Failed to load data: " + error;
+            })
+            .finally(function () {
+                vm.isBusy = false;
+            });
+
         vm.addTrip = function () {
-            vm.trips.push({ name: vm.newTrip.name, created: new Date() })
-            vm.newTrip = {};
-        }
+            vm.isBusy = true;
+            vm.errorMessage = "";
+
+            $http.post("/api/trips", vm.newTrip)
+                .then(function (response) {
+                    // Success
+                    vm.trips.push(response.data);
+                    vm.newTrip = {};
+                }, function (error) {
+                    // Failure
+                    vm.errorMessage = "Failed to save new trip";
+                })
+                .finally(function () {
+                    vm.isBusy = false;
+            });
+        };
     }
 })();
